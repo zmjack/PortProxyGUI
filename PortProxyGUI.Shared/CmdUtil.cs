8a5ee63ad371ad7074f6072c692caa01f98f5097
zmjack <jack@zmland.com>
2021-09-30 13:41:32 +0800

    v1.1.2

--- PortProxyGUI.Shared/CmdUtil.cs
+++ PortProxyGUI.Shared/CmdUtil.cs
@@ -1,6 +1,7 @@
 ﻿using NStandard;
 using PortProxyGUI.Data;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -8,6 +9,19 @@ namespace PortProxyGUI
 {
     public static class CmdUtil
     {
+        private static Regex GetRegex(string fromType, string toType)
+        {
+            return new Regex($@"{fromType}[^:]*:[^\n]+?{toType}[^:]*:\r\n\r\n.+?\r\n--------------- ----------  --------------- ----------\r\n(.+?)\r\n\r\n", RegexOptions.Singleline);
+        }
+
+        private static readonly Dictionary<string, Regex> RegexList = new Dictionary<string, Regex>()
+        {
+            ["ipv4 to ipv4"] = GetRegex("ipv4", "ipv4"),
+            ["ipv4 to ipv6"] = GetRegex("ipv4", "ipv6"),
+            ["ipv6 to ipv4"] = GetRegex("ipv6", "ipv4"),
+            ["ipv6 to ipv6"] = GetRegex("ipv6", "ipv6"),
+        };
+
         public static Rule[] GetProxies()
         {
             var output = CmdRunner.Execute("netsh interface portproxy show all");
@@ -21,8 +35,8 @@ namespace PortProxyGUI
 
             var proxies = types.SelectMany(type =>
             {
-                var typeProxies = output
-                    .ExtractFirst(new Regex($@"{type.From}:[^\n]+?{type.To}:\r\n\r\n.+?\r\n--------------- ----------  --------------- ----------\r\n(.+?)\r\n\r\n", RegexOptions.Singleline))
+                var regex = RegexList[$"{type.From} to {type.To}"];
+                var typeProxies = output.ExtractFirst(regex)
                     ?.Split(new[] { Environment.NewLine }, StringSplitOptions.None)
                     .Select(line =>
                     {
